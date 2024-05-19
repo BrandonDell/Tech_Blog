81fa739db2c9b32bd82f64438392806e00a3515e
Brandon Dell Cioppia <brandondell@brandons-mbp.lan>
2024-05-19 18:54:45 -0400

    updated and modified code from class module 14 for front js files and connection

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,57 +1,57 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
-const { withAuth } = require("../utils/auth");
+const { withAuth } = require('../utils/auth');
 
-router.get("/allPosts", async (req, res) => {
+router.get('/allPosts', async (req, res) => {
   try {
     const postData = await Post.findAll({
       limit: 20,
     });
     const post = postData.map((post) => post.get({ plain: true }));
-    console.log("!!!!!");
+    console.log('!!!!!');
     console.log(post);
-    res.render("allPosts", { post });
+    res.render('allPosts', { post });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
-router.get("/login", (req, res) => {
+router.get('/login', (req, res) => {
   // Check to see if user is logged in..truthy
   if (req.session.loggedIn) {
-    res.redirect("/");
+    res.redirect('/');
     return;
   }
   // if statement is falsy render the login in form
-  res.render("login");
+  res.render('login');
 });
 
-router.get("/signup", (req, res) => {
+router.get('/signup', (req, res) => {
   // Check to see if user is logged in..truthy
   if (req.session.loggedIn) {
-    res.redirect("/");
+    res.redirect('/');
     return;
   }
   // if statement is falsy render the login in form
-  res.render("signup", { currentPage: "Home" });
+  res.render('signup', { currentPage: 'Home' });
 });
 
-router.get("/addRecipe", (req, res) => {
+router.get('/addPost', (req, res) => {
   try {
-    res.render("addRecipe");
+    res.render('addPost');
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
-router.get("/homepage", async (req, res) => {
+router.get('/homepage', async (req, res) => {
   try {
-    const postData = await Recipe.findAll({
+    const postData = await Post.findAll({
       include: [User],
     });
     const posts = postData.map((post) => post.get({ plain: true }));
-    res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+    res.render('homepage', { posts, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -59,11 +59,10 @@ router.get("/homepage", async (req, res) => {
 });
 module.exports = router;
 
-
 // get single post router
 // router.get('/post/:id', async (req, res) => {
 //     try {
-//       // findByPk (find by primary key) 
+//       // findByPk (find by primary key)
 //       const postData = await Post.findByPk(req.params.id, {
 //         include: [
 //           User,
@@ -76,7 +75,7 @@ module.exports = router;
 //   // if found sequelize model into JS object using get/plain/true method
 //       if (postData) {
 //         const post = postData.get({ plain: true });
-  
+
 //         res.render('singlePost', { post, currentPage: 'Home' });
 //       } else {
 //         res.status(404).end();
@@ -85,6 +84,3 @@ module.exports = router;
 //       res.status(500).json(err);
 //     }
 // });
-        
-
-
